998dc1f93f728a0eef4abc485633271ba70bf76f
JDanmuji <87899411+JDanmuji@users.noreply.github.com>
2023-05-09 18:06:55 +0900

    Delete HRHZ directory

--- HRHZ/src/main/java/admin/service/AdminService.java
+++
@@ -1,6 +0,0 @@
-package admin.service;
-
-
-public interface AdminService {
-
-}
--- HRHZ/src/main/java/brand/service/BrandService.java
+++
@@ -1,9 +0,0 @@
-package brand.service;
-
-import java.util.List;
-import java.util.Map;
-
-public interface BrandService {
-    public List<Map<String, Object>> getBrandList();
-	public Map<String, Object> getBrandDetail(Map<String, Object> map);
-}
